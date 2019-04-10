310e8a8eefbe5210c8eacc2a34f596f4e8e30d85
MartinDeep13 <martin0013esp@hotmail.com>
2019-04-10 18:54:25 -0500

    servicios

--- app/src/main/java/com/example/firebaseedu/Servicios.java
+++ app/src/main/java/com/example/firebaseedu/Servicios.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -17,11 +18,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -30,7 +27,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.firebaseedu.Modelos.Usuario;
-import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.gson.Gson;
@@ -38,26 +34,22 @@ import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
-    EditaPerfil.OnFragmentInteractionListener, MetodosDePago.OnFragmentInteractionListener, Membresias.OnFragmentInteractionListener
-{
-    String uri_parse;
-    TextView usuario;
-    View btn;
-    ActionBarDrawerToggle mToggle;
-    DrawerLayout mDrawerLayout;
+public class Servicios extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        EditaPerfil.OnFragmentInteractionListener, MetodosDePago.OnFragmentInteractionListener, Membresias.OnFragmentInteractionListener, PlanchadoFragment.OnFragmentInteractionListener,
+        LavanderiaFragment.OnFragmentInteractionListener, TintoreriaFragment.OnFragmentInteractionListener, HogarFragment.OnFragmentInteractionListener {
 
-    public static FragmentManager fragmentManager;
+    DrawerLayout mDrawerLayout;
+    Button btn;
+    ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_servicios);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
         FirebaseAuth firebaseAuth;
         firebaseAuth = FirebaseAuth.getInstance();
         final FirebaseUser Users =  firebaseAuth.getCurrentUser();
-
         try {
-
             final String uid = Users.getUid();
             JSONObject datos = new JSONObject();
             datos.put("uid",uid);
@@ -65,16 +57,11 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
                     datos, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
-                    TextView nombre, usuario;
-                    Log.d("Info",response.toString());
+                    TextView usuario;
                     Gson gson = new Gson();
                     Usuario json = gson.fromJson(response.toString(),Usuario.class);
-
-                    nombre = findViewById(R.id.user);
-                    usuario = findViewById(R.id.usuarioS);
-                    nombre.setText(json.getNombre()+"!");
-                    usuario.setText(json.getNombre()+"!");
-
+                   usuario = findViewById(R.id.usuarioS);
+                   usuario.setText(json.getNombre()+"!");
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -82,21 +69,11 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
                     Log.d("Error",error.toString());
                 }
             });
-
-            RequestQueue queue = Volley.newRequestQueue(home.this);
+            RequestQueue queue = Volley.newRequestQueue(Servicios.this);
             queue.add(jsonObjectRequest);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-//        //recibir datos
-//        String name = getIntent().getStringExtra("nombre");
-//        uri_parse=getIntent().getStringExtra("uriFoto");
-//
-//        TextView user_name =  findViewById(R.id.user);
-//        user_name.setText(name);
-
-
-
 
         //quitar orientacion
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -105,54 +82,60 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-            mDrawerLayout=findViewById(R.id.drawer);
-            mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-            btn=findViewById(R.id.btn);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mDrawerLayout.openDrawer(GravityCompat.START);
+        mDrawerLayout=findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        btn=findViewById(R.id.btn_menu);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
 
-                    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-                    getSupportActionBar().setHomeButtonEnabled(true);
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setHomeButtonEnabled(true);
 
-                }
-            });
+            }
+        });
 
-            NavigationView nav = (NavigationView) findViewById(R.id.nav);
-            nav.setNavigationItemSelectedListener(this);
+        NavigationView nav = (NavigationView) findViewById(R.id.nav);
+        nav.setNavigationItemSelectedListener(this);
+
+        //NavBarBottom
 
 
 
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.planchado:
+                                selectedFragment = new  PlanchadoFragment();
+                                break;
+                            case R.id.lavanderia:
+                                selectedFragment = new  LavanderiaFragment();
+                                break;
+                            case R.id.tintoreria:
+                                selectedFragment = new  TintoreriaFragment();
+                                break;
+                            case R.id.hogar:
+                                selectedFragment = new  HogarFragment();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, new  PlanchadoFragment());
+        transaction.commit();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    private void goBack() {
-        Intent intent = new Intent(home.this,InicioSesion.class);
-        startActivity(intent);
-
-        finish();
-    }
-    public void logOut(){
-        FirebaseAuth.getInstance().signOut();
-        LoginManager.getInstance().logOut();
-        goBack();
-    }
-
-
-
-
-
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
-    {
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int id=menuItem.getItemId();
 
 
@@ -179,14 +162,10 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
             fragment = new MetodosDePago();
             FragmentSelected=true;
         }
-        else if (id == R.id.pedidos)
-        {
-           startActivity(new Intent(home.this, Servicios.class));
-        }
         else if (id == R.id.logout)
         {
             FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(home.this, InicioSesion.class));
+            startActivity(new Intent(Servicios.this, InicioSesion.class));
         }
         if ( FragmentSelected ){
 
@@ -202,15 +181,5 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
     @Override
     public void onFragmentInteraction(Uri uri) {
 
-    }
-
-    public void membresias(View view) {
-        Fragment fragment = null;
-      fragment = new  Membresias();
-        getSupportFragmentManager().beginTransaction().replace(R.id.relative,fragment).commit();
-    }
-
-    public void pedidos(View view) {
-        startActivity(new Intent(home.this,Servicios.class));
     }
 }
